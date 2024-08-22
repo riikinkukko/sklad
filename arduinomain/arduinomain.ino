f034ff9b7698ee786579226cf30d2f5089b8bdec
riikinkukko <119848942+riikinkukko@users.noreply.github.com>
2024-08-22 15:00:20 +0300

    Update arduinomain.ino

--- arduinomain/arduinomain.ino
+++ arduinomain/arduinomain.ino
@@ -1,11 +1,9 @@
-/* Написать функции:
-  load_in() -> int counter
-  load_out() -> int order
-  take_high()
-  take_low()
-*/
-
 #include <AccelStepper.h>
+#include <Servo.h>
+Servo servoRIGHT;
+Servo servoLEFT;
+Servo servoKOLENO;
+
 int counter = 0;
 int s, x, y, w, h, n; //расстояние в мм
 /*
@@ -21,15 +19,6 @@ String incomingByte;
 int counter = 0;
 int order = 0;
 String ans;
-
-#define IN1 5 //ПРАВАЯ(или левая) СЕРВА
-#define IN2 4
-#define ENA 3
-
-#define IN3 9//ЛЕВАЯ(или правая) СЕРВА
-#define IN4 7
-#define ENB 6
-//ДОПИСАТЬ КОЛЕНО!!!
 
 //пины для шаговых
 const int stepPin1 = 2;  // пин шага для двигателя 1
@@ -49,13 +38,9 @@ AccelStepper stepper3(AccelStepper::DRIVER, stepPin3, dirPin3);
 
 void setup() {
 
-  pinMode(ENA, OUTPUT); // шаг 1 сервы ПРАВАЯ(или левая, поменяем)
-  pinMode(IN1, OUTPUT); // пин 1 сервы ПРАВАЯ(или левая, поменяем)
-  pinMode(IN2, OUTPUT); // пин 1 сервы ПРАВАЯ(или левая, поменяем)
-  pinMode(ENB, OUTPUT);// шаг 2 сервы ЛЕВАЯ(или правая, поменяем)
-  pinMode(IN3, OUTPUT);// пин 2 сервы ЛЕВАЯ(или правая, поменяем)
-  pinMode(IN4, OUTPUT);// пин 2 сервы ЛЕВАЯ(или правая, поменяем)
-  //дописать колено
+  servoRIGHT.attach(3);
+  servoLEFT.attach(9);
+  servoKOLENO.attach(11);
 
   // ПОМЕНЯТЬ ПОД ПАРАМЕТРЫ /настройка скорости и ускорения шаговых двигателей
   stepper1.setMaxSpeed(200);
@@ -65,7 +50,7 @@ void setup() {
   stepper3.setMaxSpeed(200);
   stepper3.setAcceleration(100);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   
 }
 
@@ -79,68 +64,32 @@ int way_to_degrees(int way) {
 
 void move_forward(int arg) {
   /*Движение прямо*/
-
-  digitalWrite(IN1, HIGH); //1 серва(правая или левая допишем)
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH); //2 серва(правая или левая допишем)
-  digitalWrite(IN4, LOW);
-  //дописать колено
-
   int full_way = way_to_degrees(arg);
-
-  analogWrite(ENA, full_way);
-  analogWrite(ENB, full_way);
-  
+  servoRIGHT.write(full_way);//ВПИСАТЬ УГЛЫ
+  servoLEFT.write(full_way);
 }
 
 void turn_right(){
   /*Поворот направо*/
+  servoRIGHT.write(-18);//ВПИСАТЬ УГЛЫ
+  servoLEFT.write(18);
 
-  digitalWrite(IN1, HIGH); //1 серва(правая или левая допишем)
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH); //2 серва (правая или левая допишем)
-  digitalWrite(IN4, LOW);
-//дописать колено
-  
-  /*Изменить параметры*/
-  analogWrite(ENB, 18); //поменять на ENA, если ENB-правый
-  analogWrite(ENA, -18); //поменять на ENB, если ENA-правый
 }
 
 void turn_left(){
   /*Поворот налево*/
-
-  digitalWrite(IN1, HIGH); //1 серва(правая или левая допишем)
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH); //2 серва (правая или левая допишем)
-  digitalWrite(IN4, LOW);
-  //дописать колено
-
-  /*Изменить параметры*/
-  analogWrite(ENB, -18); //поменять на ENA, если ENA-правый
-  analogWrite(ENA, 18); //поменять на ENB, если ENB-правый
+  servoRIGHT.write(18);//ВПИСАТЬ УГЛЫ
+  servoLEFT.write(-18);
 }
 
 void reverse(){
   /*Функция поворота*/
-  
-  digitalWrite(IN1, HIGH); //1 серва(правая или левая допишем)
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH); //2 серва (правая или левая допишем
-  digitalWrite(IN4, LOW);
-  //дописать колено
-
-  /*Изменить параметры*/
-  analogWrite(ENB, -36);
-  analogWrite(ENA, 36);
+  servoRIGHT.write(36);//ВПИСАТЬ УГЛЫ
+  servoLEFT.write(-36);
 }
 
 void QR_low_moving_to_pos() {
-  /*Для задачи положения манипулятора для нижней полки, передачи и чтению данных с малины*/
+  /*Для задачи положения манипулятора для нижней полки*/
   // ОТРЕДАКТИРОВАТЬ  движение колен 
   stepper1.moveTo(); //в аргументы функции дописать положение
   stepper1.runToPosition();
@@ -175,7 +124,7 @@ void QR_low_moving_back() {
 }
 
 void QR_high_moving_to_pos() {
-  /*Для задачи положения манипулятора для верхней полки, передачи и чтению данных с малины*/
+  /*Для задачи положения манипулятора для верхней полки*/
 
   /*Написать движение манипулятора*/
   // ОТРЕДАКТИРОВАТЬ  движение колен 
@@ -204,6 +153,14 @@ void QR_high_moving_back() {
   stepper3.runToPosition();
 }
 
+
+/*
+Объяснение (load_out):
+  Робот должен встать в ту точку, где должна происходит выгрузка в зависимости от номера заказа (Переменная order)
+  Далее происходит выгрузка в эту точку в завимости от количества деталек в его багажнике(Переменная counter)
+*/
+
+//ЧУТЬ ЧУТЬ НЕ ПОН ЧЕ ТУТ НАДО
 /* Написать движение робота + коленей для каждого из номера заказов
 load_out(int order, int counter) {
   if (order == 1){
@@ -218,32 +175,118 @@ load_out(int order, int counter) {
 }
 */
 
+
+//ДОООООООООООООООООООООООООПИСАЛААААААААААААААААААААААААААААААААААААААААААААААААААА
 /* Написать движение коленей для каждого из каунтеров Каунтеры - кол-во коробок
 load_in(int counter){
+  servoKOLENO.write(36);//ВПИСАТЬ
   if (counter == 1){
+    stepper1.moveTo(); //в аргументы функции дописать положение
+    stepper1.runToPosition();
+
+    stepper2.moveTo(); //в аргументы функции дописать положение
+    stepper2.runToPosition();
+
+    stepper3.moveTo(); //в аргументы функции дописать положение
+    stepper3.runToPosition();
 
   }
   if (counter == 2){
+    stepper1.moveTo(); //в аргументы функции дописать положение
+    stepper1.runToPosition();
+
+    stepper2.moveTo(); //в аргументы функции дописать положение
+    stepper2.runToPosition();
+
+    stepper3.moveTo(); //в аргументы функции дописать положение
+    stepper3.runToPosition();
 
   }
   if (counter == 3){
+    stepper1.moveTo(); //в аргументы функции дописать положение
+    stepper1.runToPosition();
+
+    stepper2.moveTo(); //в аргументы функции дописать положение
+    stepper2.runToPosition();
+
+    stepper3.moveTo(); //в аргументы функции дописать положение
+    stepper3.runToPosition();
 
   }
   if (counter == 4){
+    stepper1.moveTo(); //в аргументы функции дописать положение
+    stepper1.runToPosition();
 
+    stepper2.moveTo(); //в аргументы функции дописать положение
+    stepper2.runToPosition();
+
+    stepper3.moveTo(); //в аргументы функции дописать положение
+    stepper3.runToPosition();
   }
+  delay(1000);
+  //на исходную
+  stepper1.moveTo(0);
+  stepper1.runToPosition();
+
+  stepper2.moveTo(0);
+  stepper2.runToPosition();
+
+  stepper3.moveTo(0);
+  stepper3.runToPosition();
 }*/
-/* Написать движение манипулятора
+
+//Здесь нужно больше кода. Манипулятор должен не только закинуть на верхнее звено коробку, но и поднять ее, не уронив полку
 void take_high() {
   //Для захвата коробки на верхней полке
-}
-*/
 
-/*
+  stepper1.moveTo(); //в аргументы функции дописать положение
+  stepper1.runToPosition();
+
+  stepper2.moveTo(); //в аргументы функции дописать положение
+  stepper2.runToPosition();
+
+  stepper3.moveTo(); //в аргументы функции дописать положение
+  stepper3.runToPosition();
+
+  servoKOLENO.write(180)//ВПИСАТЬ
+  delay(1000)
+//ЗАГРУЗКА В "РУКЗАЧОК" РОБОТА
+  stepper1.moveTo(0);
+  stepper1.runToPosition();
+
+  stepper2.moveTo(0);
+  stepper2.runToPosition();
+
+  stepper3.moveTo(0);
+  stepper3.runToPosition();
+}
+
+
+//Здесь нужно больше кода. Манипулятор должен не только закинуть на верхнее звено коробку, но и поднять ее, не уронив полку, а потом положить в багажник
 void take_low() {
   //Для захвата коробки на нижней полке
+  stepper1.moveTo(); //в аргументы функции дописать положение
+  stepper1.runToPosition();
+
+  stepper2.moveTo(); //в аргументы функции дописать положение
+  stepper2.runToPosition();
+
+  stepper3.moveTo(); //в аргументы функции дописать положение
+  stepper3.runToPosition();
+
+  servoKOLENO.write(180)//ВПИСАТЬ
+  delay(1000)
+//ЗАГРУЗКА В "РУКЗАЧОК" РОБОТА
+  stepper1.moveTo(0);
+  stepper1.runToPosition();
+
+  stepper2.moveTo(0);
+  stepper2.runToPosition();
+
+  stepper3.moveTo(0);
+  stepper3.runToPosition();
 }
-*/
+
 
 void loop() {
   delay(100);
