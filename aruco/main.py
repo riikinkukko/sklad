0c0d701b5d0c119fbc4fd3dd9937d4eb0c611a2d
Artem P <119848942+riikinkukko@users.noreply.github.com>
2025-02-06 18:35:07 +0300

    Add files via upload

---
+++ aruco/main.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+import cv2
+import numpy as np
+
+
+desired_aruco_dictionary = "DICT_4X4_50"
+
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL
+}
+
+obj_size = 50
+focal_length = 3.7
+def main():
+    # Check that we have a valid ArUco marker
+    if ARUCO_DICT.get(desired_aruco_dictionary, None) is None:
+        print("[INFO] ArUCo tag of '{}' is not supported".format(
+            args["type"]))
+        sys.exit(0)
+
+    # Load the ArUco dictionary
+    print("[INFO] detecting '{}' markers...".format(
+        desired_aruco_dictionary))
+    this_aruco_dictionary = cv2.aruco.Dictionary_get(ARUCO_DICT[desired_aruco_dictionary])
+    this_aruco_parameters = cv2.aruco.DetectorParameters_create()
+
+    # Start the video stream
+    cap = cv2.VideoCapture(1)
+
+    while True:
+        ret, frame = cap.read()
+
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(
+            frame, this_aruco_dictionary, parameters=this_aruco_parameters)
+
+        if len(corners) > 0:
+            ids = ids.flatten()
+
+            for (marker_corner, marker_id) in zip(corners, ids):
+                corners = marker_corner.reshape((4, 2))
+                (top_left, top_right, bottom_right, bottom_left) = corners
+                # Convert the (x,y) coordinate pairs to integers
+                top_right = (int(top_right[0]), int(top_right[1]))
+                bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+                bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+                top_left = (int(top_left[0]), int(top_left[1]))
+
+                cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
+                cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
+                cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
+                cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+
+                center_x = int((top_left[0] + bottom_right[0]) / 2.0)
+                center_y = int((top_left[1] + bottom_right[1]) / 2.0)
+                cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
+                x1_l, y1_l = top_left
+                x2_l, y2_l = bottom_left
+                x1_r, y1_r = top_right
+                x2_r, y2_r = bottom_right
+                x1 = abs(x1_l-x2_l)
+                x2 = abs(x1_r-x1_l)
+                y1 = abs(y1_l-y2_l)
+                y2 = abs(y1_r-y2_r)
+                image_size = ((x1-x2)**2+(y1-y2)**2)**0.5
+                distance = (focal_length*obj_size/image_size)*15.8
+                print(distance)
+                cv2.putText(frame, str(marker_id),
+                            (top_left[0], top_left[1] - 15),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5, (0, 255, 0), 2)
+        cv2.imshow('frame', frame)
+
+        # If "q" is pressed on the keyboard,
+        # exit this loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Close down the video stream
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    main()
